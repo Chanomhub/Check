032b926215e47e2b4e49afbed79acfb9eb3821f8
Chanomhub <121768693+Chanomhub@users.noreply.github.com>
2024-03-19 04:56:48 +0700

    Add files via upload

--- scr/Hax/main.c
+++ scr/Hax/main.c
@@ -1,17 +1,18 @@
+#include <stdio.h>
 #include <ctype.h>
-#include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 #include "hex.h"
 
-
 bool is_valid_filename(const char *filename) {
-    if (strspn(filename, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._") != strlen(filename)) {
-        return false;
+    for (int i = 0; filename[i] != '\0'; i++) {
+        if (!isalnum(filename[i]) && filename[i] != '.' && filename[i] != '_') {
+            return false;
+        }
     }
-    return true; 
+    return true;
 }
 
 bool search_in_exe(const char *filename, const uint8_t *search_data, size_t search_data_size) {
@@ -28,36 +29,42 @@ bool search_in_exe(const char *filename, const uint8_t *search_data, size_t sear
         return false;
     }
 
-  bool found = false;
+    bool found = false;
 
-  while (!feof(file)) {
-    if (fread(buffer, 1, search_data_size, file) == search_data_size) {
-      if (memcmp(buffer, search_data, search_data_size) == 0) {
-        found = true;
-        break;
-      }
+    while (!feof(file)) {
+        if (fread(buffer, 1, search_data_size, file) == search_data_size && memcmp(buffer, search_data, search_data_size) == 0) {
+            found = true;
+            break;
+        }
     }
-  }
 
-  fclose(file);
-  free(buffer);
-  return found;
+    fclose(file);
+    free(buffer);
+    return found;
 }
 
 int main() {
-  char filename[100];
-  printf("Enter the filename of the .exe program: ");
-  fgets(filename, sizeof(filename), stdin);
-  filename[strcspn(filename, "\n")] = 0;
+    char filename[100];
+    printf("Enter the filename of the .exe program: ");
+    fgets(filename, sizeof(filename), stdin);
+    filename[strcspn(filename, "\n")] = 0; 
 
-  if (is_valid_filename(filename)) {
-  bool found = search_in_exe(filename, data, sizeof(data));
-  if (found) {
-    printf("The data was found in the specified .exe program.\n");
-  } else {
-    printf("The data was not found in the specified .exe program.\n");
-  }
-  }
+    if (!is_valid_filename(filename)) {
+        printf("Invalid filename.\n");
+        return 1; 
+    }
 
-  return 0;
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        printf("Failed to open file: %s\n", filename);
+        return 1; 
+    }
+
+    bool found = search_in_exe(filename, data, sizeof(data));
+
+    fclose(file); // Always close the file
+
+    printf("The data was %s in the specified .exe program.\n", found ? "found" : "not found");
+
+    return 0;
 }
